e83ad033599498e50ad9ff271e7c04f7eb44f460
Zinah-AZ <105180491+Zinah-Zwayen@users.noreply.github.com>
2022-11-02 11:12:18 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 
 
-document.querySelector(".share-img").addEventListener("click", (e)=>{
-    document.querySelector(".porfile").classList.add("active");
-    document.querySelector(".clicked-profile").classList.remove("active");
-    document.querySelector(".share-img").addEventListener("click",(e)=>{
-        document.querySelector(".porfile").classList.remove("active");
-        document.querySelector(".clicked-profile").classList.add("active");
-    })
+// document.querySelector(".share-img").addEventListener("click", (e)=>{
+//     document.querySelector(".porfile").classList.add("active");
+//     document.querySelector(".clicked-profile").classList.remove("active");
+//     document.querySelector(".share-img").addEventListener("click",(e)=>{
+//         document.querySelector(".porfile").classList.remove("active");
+//         document.querySelector(".clicked-profile").classList.add("active");
+//     })
+// });
+
+document.querySelector(".share-img").addEventListener("click", () => { 
+    document.querySelector(".clicked-profile").classList.toggle("active");
+    document.querySelector(".porfile").classList.toggle("active"); 
+});
+
+document.querySelector("#mobile-share").addEventListener("click", () => { 
+    document.querySelector(".clicked-profile").classList.toggle("active");
+    document.querySelector(".porfile").classList.toggle("active"); 
 });
